ca77f28a0314d11361f22a5fe8ddf23fbf892184
karthikpyati <pyatikarthik@gmail.com>
2025-10-17 12:27:00 +0530

    myap

---
+++ src/main/java/com/Karthik/WebAppDeployement/WebAppDeployementApplication.java
@@ -0,0 +1,13 @@
+package com.Karthik.WebAppDeployement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppDeployementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppDeployementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Karthik/WebAppDeployement/WebAppDeployementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Karthik.WebAppDeployement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppDeployementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
